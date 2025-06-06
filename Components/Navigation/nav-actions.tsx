9e5f37e442fb59f7251cb135ba2247ea8f7c9889
binayakjoshi <binayakjoshi@users.noreply.github.com>
2025-06-07 00:33:11 +0545

    feat(nav-actions): implement toggle logic and render SideDrawer with children

---
+++ Components/Navigation/nav-actions.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import { FaUserCircle } from "react-icons/fa";
+import SideDrawer from "./side-drawer";
+import Button from "../UI/button";
+import classes from "./nav-actions.module.css";
+
+const NavActions = () => {
+  const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
+  const toggleDrawer = () => setDrawerOpen((p) => !p);
+
+  return (
+    <>
+      <Button
+        className={classes.buttonWrapper}
+        onClick={toggleDrawer}
+        aria-label="Open Menu"
+      >
+        <FaUserCircle size={30} color="#333" />
+      </Button>
+
+      <SideDrawer isOpen={drawerOpen} onClose={toggleDrawer}>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/user/slug" onClick={toggleDrawer}>
+                {/*the link will be dynamic once auth is handled with backend */}
+                <FaUserCircle size={15} /> Your Profile
+              </Link>
+            </li>
+            <li>
+              <Link href="/pets/" onClick={toggleDrawer}>
+                Your Pet Profile
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </SideDrawer>
+    </>
+  );
+};
+export default NavActions;
