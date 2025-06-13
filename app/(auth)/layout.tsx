b47bf672d852dc23be3090f6594d9439ec8ea44d
binayakjoshi <binayakjoshi@users.noreply.github.com>
2025-06-13 14:22:04 +0545

    chore(layout): fix import statements and typos

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,16 +1,15 @@
-import Footer from "@/Components/Authentication/auth-footer";
 import { ReactNode } from "react";
-import AuthFooter from "@/Components/Authentication/auth-header";
-
+import AuthHeader from "@/Components/Authentication/auth-header";
+import AuthFooter from "@/Components/Authentication/auth-footer";
 type AuthenticateLayoutProps = {
   children: ReactNode;
 };
 const AuthenticateLayout = ({ children }: AuthenticateLayoutProps) => {
   return (
     <div>
-      <AuthFooter />
+      <AuthHeader />
       {children}
-      <Footer />
+      <AuthFooter />
     </div>
   );
 };
