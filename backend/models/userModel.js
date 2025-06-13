701b68aae0fcd066253e748f6a0d78b96c5998c7
abhaya010 <bhatta.abhaya14@gmail.com>
2025-06-13 23:16:11 +0545

    Schema : (Dummy User schema written )

---
+++ backend/models/userModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, "Please provide a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: 6,
+  },
+  pets: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Pet",
+      required: true,
+    },
+  ],
+  phone: {
+    type: String,
+    trim: true,
+  },
+  address: {
+    type: String,
+    trim: true,
+  }
+})
+
+const User = mongoose.model("User", userSchema)
+
+module.exports = User
