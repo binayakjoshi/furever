62c41a15f916d0a65dac223fa82a782d0c9e8306
abhaya010 <bhatta.abhaya14@gmail.com>
2025-06-13 23:30:58 +0545

    added cors and acccept all origins

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,13 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 // Middleware
-app.use(cors())
+app.use(cors({
+     origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], 
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept", "Origin"], 
+    credentials: true, 
+}),
+)
 app.use(express.json())
 
 
