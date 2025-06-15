5fd16f12dafbd816326fe2edfcf7ebb5c34ef89a
abhaya010 <bhatta.abhaya14@gmail.com>
2025-06-16 00:49:35 +0545

    fixed connection to petController file

---
+++ backend/controllers/userController.js
@@ -0,0 +1,110 @@
+const User = require("../models/userModel")
+const Pet = require("../models/petModel")
+
+
+exports.createUser = async (req, res) => {
+  try {
+    const newUser = await User.create(req.body)
+    res.status(201).json({
+      success: true,
+      data: newUser,
+    })
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    })
+  }
+}
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find()
+    res.status(200).json({
+      success: true,
+      count: users.length,
+      data: users,
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    })
+  }
+}
+
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      data: user,
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    })
+  }
+}
+
+
+exports.updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    })
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      data: user,
+    })
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    })
+  }
+}
+
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id)
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      })
+    }
+
+    // Also delete all pets associated with this user
+    await Pet.deleteMany({ user: req.params.id })
+
+    res.status(200).json({
+      success: true,// Delete a user
+  })
+ } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    })
+  }
+}
