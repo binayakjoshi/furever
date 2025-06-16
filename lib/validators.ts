b235c9a8a59df79bab643dfcbd9b189788dcc2f3
binayakjoshi <binayakjoshi@users.noreply.github.com>
2025-06-16 23:25:58 +0545

    chore(validators): update validators for new form hook

---
+++ lib/validators.ts
@@ -0,0 +1,69 @@
+export type Validator = {
+  type: string;
+  val?: number;
+};
+
+const VALIDATOR_TYPE_REQUIRE = "REQUIRE";
+const VALIDATOR_TYPE_MINLENGTH = "MINLENGTH";
+const VALIDATOR_TYPE_MAXLENGTH = "MAXLENGTH";
+const VALIDATOR_TYPE_MIN = "MIN";
+const VALIDATOR_TYPE_MAX = "MAX";
+const VALIDATOR_TYPE_EMAIL = "EMAIL";
+const VALIDATOR_TYPE_FILE = "FILE";
+
+export const VALIDATOR_REQUIRE = (): Validator => ({
+  type: VALIDATOR_TYPE_REQUIRE,
+});
+export const VALIDATOR_FILE = (): Validator => ({ type: VALIDATOR_TYPE_FILE });
+export const VALIDATOR_MINLENGTH = (val: number): Validator => ({
+  type: VALIDATOR_TYPE_MINLENGTH,
+  val: val,
+});
+export const VALIDATOR_MAXLENGTH = (val: number): Validator => ({
+  type: VALIDATOR_TYPE_MAXLENGTH,
+  val: val,
+});
+export const VALIDATOR_MIN = (val: number): Validator => ({
+  type: VALIDATOR_TYPE_MIN,
+  val: val,
+});
+export const VALIDATOR_MAX = (val: number): Validator => ({
+  type: VALIDATOR_TYPE_MAX,
+  val: val,
+});
+export const VALIDATOR_EMAIL = (): Validator => ({
+  type: VALIDATOR_TYPE_EMAIL,
+});
+
+// main func
+export const validate = (value: string, validators: Validator[]): boolean => {
+  let isValid = true;
+
+  for (const validator of validators) {
+    if (validator.type === VALIDATOR_TYPE_REQUIRE) {
+      isValid = isValid && value.trim().length > 0;
+    }
+    if (
+      validator.type === VALIDATOR_TYPE_MINLENGTH &&
+      validator.val !== undefined
+    ) {
+      isValid = isValid && value.trim().length >= validator.val;
+    }
+    if (
+      validator.type === VALIDATOR_TYPE_MAXLENGTH &&
+      validator.val !== undefined
+    ) {
+      isValid = isValid && value.trim().length <= validator.val;
+    }
+    if (validator.type === VALIDATOR_TYPE_MIN && validator.val !== undefined) {
+      isValid = isValid && +value >= validator.val;
+    }
+    if (validator.type === VALIDATOR_TYPE_MAX && validator.val !== undefined) {
+      isValid = isValid && +value <= validator.val;
+    }
+    if (validator.type === VALIDATOR_TYPE_EMAIL) {
+      isValid = isValid && /^\S+@\S+\.\S+$/.test(value);
+    }
+  }
+  return isValid;
+};
