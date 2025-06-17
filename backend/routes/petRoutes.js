224be485833dbbcadcfb1a1432484cb0a7f8f258
abhaya010 <bhatta.abhaya14@gmail.com>
2025-06-17 21:13:05 +0545

    feat: add route to fetch all pets by user ID in petRoute.js

--- backend/routes/petRoutes.js
+++ backend/routes/petRoutes.js
@@ -5,6 +5,7 @@ const petController = require("../controllers/petController")
 // CRUD routes
 router.post("/", petController.createPet)
 router.get("/", petController.getAllPets)
+router.get("/user/:userId", petController.getAllPets) //get all petss assoicated with a user with passing userid in the URL
 router.get("/:id", petController.getPetById)
 router.put("/:id", petController.updatePet)
 router.delete("/:id", petController.deletePet)
