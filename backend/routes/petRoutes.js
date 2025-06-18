bdfe7cd8dab837c9ecdf994bc1f1a2ec21722c10
abhaya010 <bhatta.abhaya14@gmail.com>
2025-06-18 21:19:26 +0545

    made some changes in routes

--- backend/routes/petRoutes.js
+++ backend/routes/petRoutes.js
@@ -2,18 +2,12 @@ const express = require("express")
 const router = express.Router()
 const petController = require("../controllers/petController")
 
-// CRUD routes
+//Crud
 router.post("/", petController.createPet)
 router.get("/", petController.getAllPets)
-router.get("/user/:userId", petController.getAllPets) //get all petss assoicated with a user with passing userid in the URL
+router.get("/user/:userId", petController.getAllPets)
 router.get("/:id", petController.getPetById)
-router.put("/:id", petController.updatePet)
-router.delete("/:id", petController.deletePet)
-
-
-// Image upload routes
-router.post("/:petId/upload-image", imageController.uploadPetImage)
-router.post("/:petId/upload-images", imageController.uploadPetImages)
-router.delete("/:petId/delete-image", imageController.deletePetImage)
+router.put("/:id", petController.updatePet) 
+router.delete("/:id", petController.deletePet) 
 
 module.exports = router
