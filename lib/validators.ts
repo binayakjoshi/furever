8d4f701a3ffa96baf8d995d52dd6a5b0a4ae4b87
binayakjoshi <binayakjoshi@users.noreply.github.com>
2025-06-20 00:36:42 +0545

    chore(validator): add validation for type number

--- lib/validators.ts
+++ lib/validators.ts
@@ -10,6 +10,7 @@ const VALIDATOR_TYPE_MIN = "MIN";
 const VALIDATOR_TYPE_MAX = "MAX";
 const VALIDATOR_TYPE_EMAIL = "EMAIL";
 const VALIDATOR_TYPE_FILE = "FILE";
+const VALIDATOR_TYPE_NUMBER = "NUMBER";
 
 export const VALIDATOR_REQUIRE = (): Validator => ({
   type: VALIDATOR_TYPE_REQUIRE,
@@ -33,6 +34,9 @@ export const VALIDATOR_MAX = (val: number): Validator => ({
 });
 export const VALIDATOR_EMAIL = (): Validator => ({
   type: VALIDATOR_TYPE_EMAIL,
+});
+export const VALIDATOR_NUMBER = (): Validator => ({
+  type: VALIDATOR_TYPE_NUMBER,
 });
 
 // main func
@@ -63,6 +67,10 @@ export const validate = (value: string, validators: Validator[]): boolean => {
     }
     if (validator.type === VALIDATOR_TYPE_EMAIL) {
       isValid = isValid && /^\S+@\S+\.\S+$/.test(value);
+    }
+    if (validator.type === VALIDATOR_TYPE_NUMBER) {
+      const num = Number(value);
+      isValid = isValid && value.trim() !== "" && !isNaN(num);
     }
   }
   return isValid;
