a1809224bbdfb212dab10a1c23e542f645831c47
abhaya010 <bhatta.abhaya14@gmail.com>
2025-06-21 12:47:46 +0545

    feat:addedd routes for login and signup functions

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,41 @@
 const express = require("express")
 const router = express.Router()
 const userController = require("../controllers/userController")
+const { body } = require("express-validator")
+const HttpError = require("../models/http-error")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+router.post(
+  "/signup",
+  [
+    body("name").not().isEmpty().withMessage("Name is required"),
+    body("email").isEmail().withMessage("Please enter a valid email"),
+    body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters"),
+    body("role").optional().isIn(["user", "vet"]).withMessage("Role must be either user or vet"),
+  ],
+  userController.signup,
+)
+
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Please enter a valid email"),
+    body("password").not().isEmpty().withMessage("Password is required"),
+  ],
+  userController.login,
+)
 
 // CRUD routes
-router.post("/", userController.createUser)
+router.post(
+    "/signup", userController.createUser)
 router.get("/", userController.getAllUsers)
 router.get("/:id", userController.getUserById)
 router.put("/:id", userController.updateUser)
 router.delete("/:id", userController.deleteUser)
+
+// Profile image routes
+// router.post("/:userId/upload-profile-image", imageController.uploadUserImage)
+// router.delete("/:userId/delete-profile-image", imageController.deleteUserImage)
 
 module.exports = router
