c488f5ca125b356bb6268d975f8e3296953c0379
binayakjoshi <binayakjoshi@users.noreply.github.com>
2025-06-21 19:25:57 +0545

    chore(authentication): split the token as required

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -1,23 +1,20 @@
-const jwt = require ('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const HttpError = require('../models/http-error');
 
-const HttpError= require('../models/http-error');
 
-const authenticate= (req, res,next)=>{
-    try{
-        const token = req.headers.authentication;
+const authenticate = (req,res,next)=> {
+	try{
+		const token = req.headers.authorization.split(' ')[1];
+		if(!token){
+			return next(new HttpError('please login or create account to visit this route',401));
+		}
+		const decodedToken = jwt.verify(token,process.env.JWT_SECRET);
+		req.userData = {userId: decodedToken.userId};
+		next();
 
-        if(!token || !token.startsWith('Bearer')){
-            return next(new HttpError('Authentication failed, token not found', 401));
-        }
-
-        if (!token){
-            return next(new HttpError('Authentication failed, token not found', 401));
-    }
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    req.userData={userId:decodedToken.userId}
-    next();
+	}catch(error){
+		return next(new HttpError('Please Login First.',401));
+	}
+	
 }
-    catch(error){
-        return next(new HttpError('Authentication failed, token not found', 401));
-    }
-}
+module.exports = authenticate;
