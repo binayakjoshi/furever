d9fd239828d3a33a223935f9921769ef67e523ad
abhaya010 <bhatta.abhaya14@gmail.com>
2025-06-23 22:15:28 +0545

    feat:added authentication for post request

--- backend/routes/petRoutes.js
+++ backend/routes/petRoutes.js
@@ -3,10 +3,11 @@ const router = express.Router()
 const petController = require("../controllers/petController")
 const imageUpload = require("../middleware/imageUpload")
 const { body } = require("express-validator") 
-
+const authenticate = require("../middleware/authentication") 
 
 router.post(
   "/",
+  authenticate, // Ensure user is authenticated
   imageUpload.single("image"), [
     body("name").not().isEmpty().withMessage("Pet name is required"),
     body("description").not().isEmpty().withMessage("Pet description is required"),
