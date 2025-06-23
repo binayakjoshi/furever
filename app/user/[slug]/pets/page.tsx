de3b61962d3fd0fe937cc5f0b0982c5cec2d8775
binayakjoshi <binayakjoshi@users.noreply.github.com>
2025-06-23 22:56:38 +0545

    chore(page): add backend integration logic

--- app/user/[slug]/pets/page.tsx
+++ app/user/[slug]/pets/page.tsx
@@ -1,11 +1,37 @@
+import { cookies } from "next/headers";
 import PetList from "@/Components/Pets/pet-list";
-import { dummyPets } from "@/lib/dummy-data";
 
-const YourPetPage = () => {
+type YourPetPageProps = {
+  params: Promise<{ slug?: string }>;
+};
+
+const YourPetPage = async ({ params }: YourPetPageProps) => {
+  const { slug } = await params;
+  const cookieHeader = (await cookies()).toString();
+  let pets = [];
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/pets/user/${slug}`,
+      {
+        headers: {
+          Cookie: cookieHeader,
+        },
+        cache: "no-store",
+      }
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch pets");
+    }
+    pets = await response.json();
+  } catch (error) {
+    console.log("Error:", error);
+    throw new Error("Cannot get the pet list");
+  }
+
   return (
     <div style={{ padding: "16px" }}>
       <h1>Your Pets</h1>
-      <PetList pets={dummyPets} />
+      <PetList pets={pets.data} />
     </div>
   );
 };
