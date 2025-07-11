ac7e5d8cdbb49d7fcfe35186cb922319c0c34c3d
abhaya010 <bhatta.abhaya14@gmail.com>
2025-07-11 23:52:41 +0545

    ..

--- backend/routes/vetRoutes.js
+++ backend/routes/vetRoutes.js
@@ -18,11 +18,6 @@ router.get("/", veterinarianController.getAllVeterinarians)
 
 router.get("/:id", veterinarianController.getVeterinarianById)
 
-// // Get veterinarians by specialization
-// router.get("/specialization/:specialization", veterinarianController.getVeterinariansBySpecialization)
-
-// // Search veterinarians by location
-// router.get("/search/location", veterinarianController.searchVeterinariansByLocation)
 
 //for logged in users only
 router.use(authenticate)
