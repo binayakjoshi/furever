9af6895d0e1cc4876dd0f171406c794dc879e259
binayakjoshi <binayakjoshi@users.noreply.github.com>
2025-07-13 13:27:36 +0545

    feat(adoption): add routes for showing interest in adoption

--- app/api/adoption/[adoptSlug]/route.ts
+++ app/api/adoption/[adoptSlug]/route.ts
@@ -23,3 +23,31 @@ export const GET = async (
     throw new Error("could not get the adoption list");
   }
 };
+
+export const POST = async (
+  request: NextRequest,
+  { params }: { params: { adoptSlug: string } }
+) => {
+  try {
+    const { adoptSlug } = await params;
+
+    const cookieHeader = request.headers.get("cookie") || "";
+    const backendRes = await fetch(
+      `${process.env.BACKEND_URL}/api/adoptions/${adoptSlug}/interest`,
+      {
+        method: "POST",
+        headers: {
+          Cookie: cookieHeader,
+          "Content-Type": "application/json",
+        },
+        body: null,
+      }
+    );
+
+    const data = await backendRes.json();
+    return NextResponse.json(data, { status: backendRes.status });
+  } catch (err) {
+    console.log(err);
+    throw new Error("could not get the adoption list");
+  }
+};
