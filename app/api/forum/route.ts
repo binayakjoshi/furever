3540b08dfafd0bac7d8f6943878bc76ed0cf8d08
binayakjoshi <binayakjoshi@users.noreply.github.com>
2025-07-15 00:22:09 +0545

    feat(route.ts): add routes for hanlding new forum post

--- app/api/forum/route.ts
+++ app/api/forum/route.ts
@@ -20,3 +20,30 @@ export const GET = async (request: NextRequest) => {
     );
   }
 };
+export const POST = async (request: NextRequest) => {
+  try {
+    const cookieHeader = request.headers.get("cookie") || "";
+
+    const body = await request.json();
+
+    const backendRes = await fetch(`${process.env.BACKEND_URL}/api/forum`, {
+      method: "POST",
+      headers: {
+        Cookie: cookieHeader,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await backendRes.json();
+    const nextRes = NextResponse.json(data, { status: backendRes.status });
+
+    return nextRes;
+  } catch (err) {
+    console.error("create forum post proxy error:", err);
+    return NextResponse.json(
+      { success: false, message: "Proxy error" },
+      { status: 500 }
+    );
+  }
+};
