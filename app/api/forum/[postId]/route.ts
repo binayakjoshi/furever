85a2cf3127e1e912bb0e302619d6ce09727eb893
binayakjoshi <binayakjoshi@users.noreply.github.com>
2025-07-17 14:56:56 +0545

    feat(route.ts): add delete and put route to edit/delete a post

--- app/api/forum/[postId]/route.ts
+++ app/api/forum/[postId]/route.ts
@@ -61,3 +61,58 @@ export const POST = async (request: NextRequest, { params }: ParamsType) => {
     );
   }
 };
+
+export const DELETE = async (request: NextRequest, { params }: ParamsType) => {
+  try {
+    const { postId } = await params;
+    const cookieHeader = request.headers.get("cookie") || "";
+
+    const backendRes = await fetch(
+      `${process.env.BACKEND_URL}/api/forum/${postId}`,
+      {
+        method: "DELETE",
+        headers: { Cookie: cookieHeader },
+      }
+    );
+    const data = await backendRes.json();
+    const nextRes = NextResponse.json(data, { status: backendRes.status });
+
+    const setCookie = backendRes.headers.get("set-cookie");
+    if (setCookie) nextRes.headers.set("set-cookie", setCookie);
+
+    return nextRes;
+  } catch (err) {
+    console.error("delete forum post proxy erro:", err);
+    return NextResponse.json(
+      { success: false, message: "Proxy error" },
+      { status: 500 }
+    );
+  }
+};
+export const PUT = async (request: NextRequest, { params }: ParamsType) => {
+  try {
+    const { postId } = await params;
+    const body = await request.json();
+    const cookieHeader = request.headers.get("cookie") || "";
+    const backendRes = await fetch(
+      `${process.env.BACKEND_URL}/api/forum/${postId}`,
+      {
+        method: "PUT",
+        headers: {
+          Cookie: cookieHeader,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      }
+    );
+    const data = await backendRes.json();
+    const nextRes = NextResponse.json(data, { status: backendRes.status });
+    return nextRes;
+  } catch (err) {
+    console.error("edit forum post proxy error:", err);
+    return NextResponse.json(
+      { success: false, message: "Proxy error" },
+      { status: 500 }
+    );
+  }
+};
