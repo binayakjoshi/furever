f133a24b8509e5e0f23300850f173123b34c736a
binayakjoshi <binayakjoshi@users.noreply.github.com>
2025-07-19 10:43:20 +0545

    feat(route):add routes to edit and delete vet details

--- app/api/vets/[vetId]/route.ts
+++ app/api/vets/[vetId]/route.ts
@@ -21,3 +21,31 @@ export const GET = async (request: NextRequest, { params }: ParamType) => {
     );
   }
 };
+
+export const DELETE = async (request: NextRequest, { params }: ParamType) => {
+  try {
+    const { vetId } = await params;
+    const cookieHeader = request.headers.get("cookie") || "";
+
+    const backendRes = await fetch(
+      `${process.env.BACKEND_URL}/api/users/${vetId}`,
+      {
+        method: "DELETE",
+        headers: { Cookie: cookieHeader },
+      }
+    );
+    const data = await backendRes.json();
+    const nextRes = NextResponse.json(data, { status: backendRes.status });
+
+    const setCookie = backendRes.headers.get("set-cookie");
+    if (setCookie) nextRes.headers.set("set-cookie", setCookie);
+
+    return nextRes;
+  } catch (err) {
+    console.error("user delete proxy error:", err);
+    return NextResponse.json(
+      { success: false, message: "Proxy error" },
+      { status: 500 }
+    );
+  }
+};
