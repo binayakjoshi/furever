5f89cf08fe597d0d703a639f0745cd1b10543887
binayakjoshi <binayakjoshi@users.noreply.github.com>
2025-07-31 10:20:17 +0545

    feat(route):  update route to book appointments

--- app/api/vets/[vetId]/route.ts
+++ app/api/vets/[vetId]/route.ts
@@ -49,3 +49,31 @@ export const DELETE = async (request: NextRequest, { params }: ParamType) => {
     );
   }
 };
+
+export const POST = async (request: NextRequest) => {
+  try {
+    const cookieHeader = request.headers.get("cookie") || "";
+
+    const backendRes = await fetch(
+      `${process.env.BACKEND_URL}/api/appointments`,
+      {
+        method: "POST",
+        headers: {
+          Cookie: cookieHeader,
+        },
+        body: null,
+      }
+    );
+
+    const data = await backendRes.json();
+    const nextRes = NextResponse.json(data, { status: backendRes.status });
+
+    return nextRes;
+  } catch (err) {
+    console.error("Add Pet proxy error:", err);
+    return NextResponse.json(
+      { success: false, message: "Proxy error" },
+      { status: 500 }
+    );
+  }
+};
